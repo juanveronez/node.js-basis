ba70b1e59ec902fddc3173e1c05c63f350a6810a
Juan Veronez <juan.monteirov@gmail.com>
2025-06-01 11:47:29 -0300

    feat: use method and url from req object to segregate code paths

--- src/server.js
+++ src/server.js
@@ -1,7 +1,21 @@
 import http from 'node:http'
 
 const server = http.createServer((req, res) => {
-  res.end('Hello Node.JS!')
+  const { method, url } = req
+
+  if (url === '/users' && method === 'GET') {
+    return res.end('List users')
+  }
+
+  if (url === '/users' && method === 'POST') {
+    return res.end('Create user')
+  }
+
+  if (url === '/users' && method === 'DELETE') {
+    return res.end('Delete user')
+  }
+
+  res.end(method, url)
 })
 
 server.listen(3333)
