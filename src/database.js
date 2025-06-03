0f05edb7cd0691ce77a815c350d9c38469412093
Juan Veronez <juan.monteirov@gmail.com>
2025-06-03 18:06:33 -0300

    feat: Create an JSON file database using URL to def. an abs. path

--- src/database.js
+++ src/database.js
@@ -1,6 +1,24 @@
+import fs from 'node:fs/promises'
+
+// import.meta.url is the absolute path to this module
+// new URL creates a path that should be used to ref an absolute location
+const databasePath = new URL('../db.json', import.meta.url)
+
 export class Database {
   // # set something as private
   #database = {}
+
+  constructor() {
+    fs.readFile(databasePath, 'utf8')
+      .then(data => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => this.#persist())
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
 
   select(entity) {
     const data = this.#database[entity] ?? []
@@ -13,6 +31,8 @@ export class Database {
     } else {
       this.#database[entity] = [data]
     }
+
+    this.#persist()
 
     return data
   }
