95fd89883251a0bf07ff0c856eb839b3dc42b0d0
Juan Veronez <juan.monteirov@gmail.com>
2025-06-04 08:07:42 -0300

    feat: create a query params extractor and update route path builder

--- src/utils/buildRoutePath.js
+++ src/utils/buildRoutePath.js
@@ -5,7 +5,7 @@ export function buildRoutePath(path) {
   // 1$ significa que usamos o valor do Regex anterior como nome do grupo
   const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
 
-  const pathRegex = new RegExp(`^${pathWithParams}`)
+  const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`)
 
   return pathRegex
 }
