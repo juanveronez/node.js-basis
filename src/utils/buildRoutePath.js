bbd4b0a61c4f9435107926b1b923a179b4f1559a
Juan Veronez <juan.monteirov@gmail.com>
2025-06-04 07:41:09 -0300

    feat: create a RegEx to match URLs with params and return the params in req

---
+++ src/utils/buildRoutePath.js
@@ -0,0 +1,11 @@
+export function buildRoutePath(path) {
+  const routeParametersRegex = /:([a-zA-Z]+)/g
+
+  // ? é usado para definir o nome de um (grupo) no RegEx
+  // 1$ significa que usamos o valor do Regex anterior como nome do grupo
+  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
+
+  const pathRegex = new RegExp(`^${pathWithParams}`)
+
+  return pathRegex
+}
